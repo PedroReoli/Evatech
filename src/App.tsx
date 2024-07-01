880c6200428c94e4e2907e1f7bd3869580ce8895
PedroReoli <pedrolucas2012@gmail.com>
2024-07-01 20:33:50 -0300

    Routing Complete

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import { Routes, Route } from 'react-router-dom';
+import './globals.css';
+// Autenticação 
+import SigninForm  from './_auth/forms/SigninForm';
+import SignupForm from './_auth/forms/SignupForm';
+import AuthLayout from './_auth/AuthLayout';
+// Páginas 
+// Devido ao index.ts ,agora dá para importar mais facil 
+// fica assim agora {pagina1,pagina2,pagina3} from  './_root/pages/'
+import { Home } from './_root/pages/';
+import RootLayout from './_root/RootLayout';
+const App = () => {
+  return (
+    <main className='flex h-screen'>
+        <Routes>
+            {/* Rotas Públicas  - Sem login */}
+            <Route element={<AuthLayout />}>    {/* Páginas Sobrepostas */}
+                <Route path="/sign-in" element={<SigninForm/>}/>   
+                <Route path="/sign-up" element={<SignupForm/>}/>  
+            </Route>
+            {/* Rotas Privadas  - Com Login */}
+            <Route element={<RootLayout />}>    {/* Páginas Sobrepostas */}
+                 <Route index element={<Home/>}/>    
+            </Route>
+        </Routes>
+
+    </main>    
+
+)
+}
+
+export default App
