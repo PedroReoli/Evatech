1fd0266353e8ae65b756206d573c6fc7a62e3ca1
PedroReoli <pedrolucas2012@gmail.com>
2024-07-02 13:56:02 -0300

    b

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -17,7 +17,7 @@ const AuthLayout = () => {
             </section>
 
             <img
-              src="/assets/images/project-image.svg"
+              // src="/assets/images/project-image.svg"
               alt="logo"
               className="hidden xl:block h-screen w-1/2 object-cover bg-no-repeat"
           />
--- tailwind.config.js
+++ tailwind.config.js
@@ -60,6 +60,9 @@ module.exports = {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
       },
+      backgroundImage: {
+        'left-img': "url('/assets/images/background-auth.svg')",
+      },
     },
   },
   plugins: [require('tailwindcss-animate')],
