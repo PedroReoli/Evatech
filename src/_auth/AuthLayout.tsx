99de26ea9028f900e8cefdf291c1b7ed152c7d64
PedroReoli <pedrolucas2012@gmail.com>
2024-07-01 21:11:34 -0300

    AuthLayout.tsx

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import { Outlet,Navigate } from "react-router-dom"
+
+
 
 const AuthLayout = () => {
+  const isAuthenticated = false;
+
   return (
-    <div>AuthLayout</div>
+    <>
+    {/* Caso esteja autenticado(logado) ....vai redirecionar direto pro index ( no caso o Home ) */}
+      {isAuthenticated ? (
+        <Navigate to= "/" />
+      ): (
+        <>
+            <section className="flex flex-1 justify-center items-center flex-col py-10">
+             <Outlet />
+            </section>
+
+            <img
+              src="/assets/images/side-img.svg"
+              alt="logo"
+              className="hidden xl:block h-screen w-1/2 object-cover bg-no-repeat"
+          />
+        </>
+      )}
+    </>
   )
 }
 
