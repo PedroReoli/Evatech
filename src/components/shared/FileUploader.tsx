f0371ea40a19a0810bec0f183226e963e1e6a3ef
PedroReoli <pedrolucas2012@gmail.com>
2024-07-16 16:33:14 -0300

    Update FileUploader.tsx

--- src/components/shared/FileUploader.tsx
+++ src/components/shared/FileUploader.tsx
@@ -1,6 +1,5 @@
 import { useCallback, useState } from "react";
 import { FileWithPath, useDropzone } from "react-dropzone";
-
 import { Button } from "@/components/ui/button";
 import { convertFileToUrl } from "@/lib/utils";
 
@@ -15,11 +14,13 @@ const FileUploader = ({ fieldChange, mediaUrl }: FileUploaderProps) => {
 
   const onDrop = useCallback(
     (acceptedFiles: FileWithPath[]) => {
-      setFile(acceptedFiles);
-      fieldChange(acceptedFiles);
-      setFileUrl(convertFileToUrl(acceptedFiles[0]));
+      if (acceptedFiles.length > 0) {
+        setFile(acceptedFiles);
+        fieldChange(acceptedFiles);
+        setFileUrl(convertFileToUrl(acceptedFiles[0]));
+      }
     },
-    []
+    [fieldChange]
   );
 
   const { getRootProps, getInputProps } = useDropzone({
@@ -33,13 +34,14 @@ const FileUploader = ({ fieldChange, mediaUrl }: FileUploaderProps) => {
   return (
     <div
       {...getRootProps()}
-      className="flex flex-center flex-col bg-white rounded-xl cursor-pointer border-2 border-purple-1">
+      className="flex flex-center flex-col bg-white rounded-xl cursor-pointer border-2 border-purple-1"
+    >
       <input {...getInputProps()} className="cursor-pointer" />
 
       {fileUrl ? (
         <>
           <div className="flex flex-1 justify-center w-full p-5 lg:p-10">
-            {file[0].type === "application/pdf" ? (
+            {file[0]?.type === "application/pdf" ? (
               <embed src={fileUrl} width="100%" height="600px" type="application/pdf" />
             ) : (
               <img src={fileUrl} alt="uploaded file" className="file_uploader-img" />
@@ -48,19 +50,17 @@ const FileUploader = ({ fieldChange, mediaUrl }: FileUploaderProps) => {
           <p className="file_uploader-label">Clique ou arraste uma foto ou PDF</p>
         </>
       ) : (
-        <div className="file_uploader-box ">
+        <div className="file_uploader-box">
           <img
             src="/assets/icons/file-upload.svg"
             width={96}
             height={77}
             alt="file upload"
           />
-
           <h3 className="base-medium text-light-2 mb-2 mt-6">
             Coloque seu arquivo aqui
           </h3>
           <p className="text-light-4 small-regular mb-6">SVG, PNG, JPG, PDF</p>
-
           <Button type="button" className="shad-button_dark_4">
             Selecionar nos arquivos locais
           </Button>
