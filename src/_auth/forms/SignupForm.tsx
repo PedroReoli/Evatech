081433168fa39c6c4bd8104efda03bb9a3e87f68
PedroReoli <pedrolucas2012@gmail.com>
2024-07-16 21:21:49 -0300

    Update SignupForm.tsx

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -81,7 +81,7 @@ const SignupForm = () => {
           Crie uma conta nova
         </h2>
         <p className="text-light-3 small-medium md:base-regular mt-2">
-          ( COLOCAR UM LEMA DO PROJETO )
+         Empoderando Mulheres, Transformando Vidas
         </p>
 
         <form
