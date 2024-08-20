9c2a5503fdc9b4710b43a68a930c17dd66c71d24
PedroReoli <pedrolucas2012@gmail.com>
2024-08-20 12:34:45 -0300

    Update index.ts

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,61 +1,62 @@
-export const sidebarLinks = [
-    {
-      imgURL: "/assets/icons/home.svg",
-      route: "/",
-      label: "Início",
-    },
-    {
-      imgURL: "/assets/icons/people.svg",
-      route: "/all-users",
-      label: "Participantes",
-    },
-    {
-      imgURL: "/assets/icons/wallpaper.svg",
-      route: "/explore",
-      label: "Material",
-    },
-    {
-      imgURL: "/assets/icons/bookmark.svg",
-      route: "/saved",
-      label: "Concluído",
-    },
-    {
-      imgURL: "/assets/icons/gallery-add.svg",
-      route: "/create-post",
-      label: "Postar",
-    },
-   
-  
-  ];
-  
-  export const bottombarLinks = [
-    {
-      imgURL: "/assets/icons/home.svg",
-      route: "/",
-      label: "Início",
-    },
-    {
-      imgURL: "/assets/icons/people.svg",
-      route: "/all-users",
-      label: "Participantes",
-    },
-    {
-      imgURL: "/assets/icons/wallpaper.svg",
-      route: "/explore",
-      label: "Material",
-    },
-    {
-      imgURL: "/assets/icons/bookmark.svg",
-      route: "/saved",
-      label: "Concluído",
-    },
-    {
-      imgURL: "/assets/icons/gallery-add.svg",
-      route: "/create-post",
-      label: "Postar",
-    },
-   
-  
-  ];
-  
-  
+interface NavLink {
+  imgURL: string;
+  route: string;
+  label: string;
+}
+
+export const sidebarLinks: NavLink[] = [
+  {
+    imgURL: "/assets/icons/home.svg",
+    route: "/",
+    label: "Início",
+  },
+  {
+    imgURL: "/assets/icons/people.svg",
+    route: "/all-users",
+    label: "Participantes",
+  },
+  {
+    imgURL: "/assets/icons/wallpaper.svg",
+    route: "/explore",
+    label: "Material",
+  },
+  {
+    imgURL: "/assets/icons/bookmark.svg",
+    route: "/saved",
+    label: "Concluído",
+  },
+  {
+    imgURL: "/assets/icons/gallery-add.svg",
+    route: "/create-post",
+    label: "Postar",
+  },
+];
+
+export const bottombarLinks: NavLink[] = [
+  {
+    imgURL: "/assets/icons/home.svg",
+    route: "/",
+    label: "Início",
+  },
+  {
+    imgURL: "/assets/icons/people.svg",
+    route: "/all-users",
+    label: "Participantes",
+  },
+  {
+    imgURL: "/assets/icons/wallpaper.svg",
+    route: "/explore",
+    label: "Material",
+  },
+  {
+    imgURL: "/assets/icons/bookmark.svg",
+    route: "/saved",
+    label: "Concluído",
+  },
+  {
+    imgURL: "/assets/icons/gallery-add.svg",
+    route: "/create-post",
+    label: "Postar",
+  },
+];
+// Polirmofismo paramétrico
